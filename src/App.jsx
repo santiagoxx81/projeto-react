705c0ecdeb5776f3fafae8918990778e00173b17
santiagoxx81 <santiagoxx81@hotmail.com>
2025-05-13 15:33:43 -0300

    adicionadas quebras de linha no texto main

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,17 @@ function App() {
       <p>2º SGT PM - aluno do 1º CADSPM</p>
       </header>
       <main>
-        <h2>Sobre mim</h2>
-        <p>Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit.
-          Ipsum quam ullam dolore perspiciatis delectus ad dolores
-          quas nemo aut vero, error, consequuntur, odit labore?
+        <h2>Sobre mim</h2> <br />
+        <p>Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. <br />
+          Ipsum quam ullam dolore perspiciatis delectus ad dolores <br />
+          quas nemo aut vero, error, consequuntur, odit labore? <br />
           Cum, vel! Vero, harum. Provident, totam.</p>
-        
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Atque, qui reiciendis molestiae eligendi culpa, magnam
-          odit necessitatibus nemo, dicta esse sequi doloremque
-          unde eum hic? Animi sint tenetur qui eaque.</p>
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+          Atque, qui reiciendis molestiae eligendi culpa, magnam<br />
+          odit necessitatibus nemo, dicta esse sequi doloremque<br />
+          unde eum hic? Animi sint tenetur qui eaque.</p> <br />
+
         <h3>habilidades principais</h3>
         <ul>
           <li>Montagem e configuração de PCs</li>
