0e43f509d50ef85e095b5280627396866f372560
santiagoxx81 <santiagoxx81@hotmail.com>
2025-05-13 15:06:28 -0300

    primeiro commit do appvite

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <header>
+      <h1>Jonathan Felipe</h1>
+      <p>2º SGT PM - aluno do 1º CADSPM</p>
+      </header>
+      <main>
+        <h2>Sobre mim</h2>
+        <p>Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit.
+          Ipsum quam ullam dolore perspiciatis delectus ad dolores
+          quas nemo aut vero, error, consequuntur, odit labore?
+          Cum, vel! Vero, harum. Provident, totam.</p>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Atque, qui reiciendis molestiae eligendi culpa, magnam
+          odit necessitatibus nemo, dicta esse sequi doloremque
+          unde eum hic? Animi sint tenetur qui eaque.</p>
+        <h3>habilidades principais</h3>
+        <ul>
+          <li>Montagem e configuração de PCs</li>
+          <li>Redes e conectividades</li>
+          <li>Sistemas Operacionais (windows, linux, MS-DOS</li>
+          <li>Didática </li>
+        </ul>
+
+
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
